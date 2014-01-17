e702852b529911623e60be29f0c3cd7db4a99894
Marius Mandal <mariusmandal@gmail.com>
2014-01-17 17:02:37 +0100

    stengt

--- server_upgrade.php
+++ server_upgrade.php
@@ -15,5 +15,5 @@ html {
 </style>
 
 <h1>UKM-TV ikke tilgjengelig</h1>
-<p>Grunnet serveroppgradering er UKM-TV utilgjengelig mandag 13.01 - torsdag 16.01</p>
+<p>Grunnet serveroppgradering er UKM-TV utilgjengelig t.o.m mandag 20.01</p>
 <?php die(); ?>
