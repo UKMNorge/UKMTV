8c9710b36c9b3d2f268749e6d889da3a90eb3234
Tarjei Husøy <git@thusoy.com>
2014-08-13 08:50:04 +0000

    Fix tablename

--- caches/heartbeat.php
+++ caches/heartbeat.php
@@ -13,25 +13,25 @@ require_once('UKM/sql.class.php');
 $cache_id = $_POST['cache_id'];
 $cache_ip = $_SERVER['REMOTE_ADDR'];
 if ( !$cache_id ) {
-	$sql = new SQLins('ukmtv_caches_caches');
+	$sql = new SQLins('ukm_tv_caches_caches');
 	$sql->add('ip', $cache_ip);
 	$sql->add('status', 'OK');
 	$results = $sql->run();
 	$cache_id = $sql->insid();
 } else {
-	$select_sql = new SQL("SELECT id FROM `ukmtv_caches_caches` WHERE `id`='#id'", array('id' => $cache_id));
+	$select_sql = new SQL("SELECT id FROM `ukm_tv_caches_caches` WHERE `id`='#id'", array('id' => $cache_id));
 	$res = $select_sql->run( $select_sql );
 	$res = mysql_fetch_assoc( $res );
 	if ( !$res ) {
 		error_log("Got heartbeat from unknown id. ID=$cache_id, ip=$cache_ip.");
-		$insert_new = new SQLins('ukmtv_caches_caches');
+		$insert_new = new SQLins('ukm_tv_caches_caches');
 		$insert_new->add('id', $cache_id);
 		$insert_new->add('ip', $cache_ip);
 		$insert_new->add('status', 'OK');
 		$insert_new->run();
 	}
 	error_log("Updating status for ip $cache_ip");
-	$sql = new SQLins('ukmtv_caches_caches', array('id' => $cache_id));
+	$sql = new SQLins('ukm_tv_caches_caches', array('id' => $cache_id));
 	$sql->add('ip', $cache_ip);
 	$sql->run();
 }
