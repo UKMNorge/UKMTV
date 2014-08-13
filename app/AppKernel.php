cb8582a035ee2a40cc6a7ec7f3617c717286802b
Marius Mandal <mariusmandal@gmail.com>
2014-08-13 14:31:45 +0000

    Working setup of ukm tv

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,6 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
@@ -33,4 +32,15 @@ class AppKernel extends Kernel
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function getCacheDir()
+    {
+        return '/tmp/symfony/ukmtv/'.$this->environment.'/cache';
+    }
+    
+    public function getLogDir()
+    {
+        return '/tmp/symfony/ukmtv/'.$this->environment.'/logs';
+    }
+
 }
