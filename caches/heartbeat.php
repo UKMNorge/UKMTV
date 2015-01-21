e770f49fd4efbe3be5e18fa00f1f19c2172772db
Tarjei Husøy <git@thusoy.com>
2015-01-21 14:38:56 +0100

    Remove space-related fields from cache heartbeat

--- caches/heartbeat.php
+++ caches/heartbeat.php
@@ -7,27 +7,22 @@ require_once('UKMconfig.inc.php');
 require_once('../tvconfig.php');
 require_once('UKM/sql.class.php');
 
-//echo "Here we are...";
-
 // Fetch the data about the cache reporting in
 $cache_id = $_POST['cache_id'];
-$cache_space_total = $_POST['space_total'];
-$cache_space_used = $_POST['space_used'];
 $cache_status = $_POST['status'];
-if ( !($cache_space_total && $cache_space_used && $cache_status) ) {
+if ( !$cache_status ) {
 	http_response_code(400);
-	error_log("Got invalid heartbeat, data was cache_id=$cache_id, space_total=$cache_space_total, space_used=$cache_space_used, status=$cache_status");
+	error_log("Got invalid heartbeat, data was cache_id=$cache_id, status=$cache_status");
 	die(json_encode(array(
 		'message' => 'One or more required fields missing.',
 	)));
 }
+
 $cache_ip = $_SERVER['REMOTE_ADDR'];
 if ( !$cache_id ) {
 	$sql = new SQLins('ukm_tv_caches_caches');
 	$sql->add('ip', $cache_ip);
 	$sql->add('status', $cache_status);
-	$sql->add('space_total', $cache_space_total);
-	$sql->add('space_used', $cache_space_used);
 	$results = $sql->run();
 	$cache_id = $sql->insid();
 } else {
@@ -40,16 +35,12 @@ if ( !$cache_id ) {
 		$insert_new->add('id', $cache_id);
 		$insert_new->add('ip', $cache_ip);
 		$insert_new->add('status', $cache_status);
-		$insert_new->add('space_total', $cache_space_total);
-		$insert_new->add('space_used', $cache_space_used);
 		$insert_new->run();
 	}
 	error_log("Updating status for ip $cache_ip");
 	$sql = new SQLins('ukm_tv_caches_caches', array('id' => $cache_id));
 	$sql->add('ip', $cache_ip);
 	$sql->add('status', $cache_status);
-	$sql->add('space_total', $cache_space_total);
-	$sql->add('space_used', $cache_space_used);
 	$sql->add('last_heartbeat',  date('Y-m-d G:i:s'));
 	$sql->run();
 }
@@ -58,6 +49,4 @@ die(json_encode(array(
 	'cache_id' => $cache_id,
 	'status' => $cache_status,
 	'ip' => $cache_ip,
-	'space_total' => $cache_space_total,
-	'space_used' => $cache_space_used,
 )));
--- caches/index.php
+++ caches/index.php
@@ -7,7 +7,7 @@ require_once('UKMconfig.inc.php');
 require_once('../tvconfig.php');
 require_once('UKM/sql.class.php');
 
-$caches_query = new SQL("SELECT `id`, `ip`, `status`, `last_heartbeat`, `space_total`, `space_used`  FROM `ukm_tv_caches_caches`");
+$caches_query = new SQL("SELECT `id`, `ip`, `status`, `last_heartbeat` FROM `ukm_tv_caches_caches`");
 $res = $caches_query->run( $caches_query );
 
 ?>
@@ -23,31 +23,14 @@ $res = $caches_query->run( $caches_query );
 
 <?php
 
-function humanizeSize($size) {
-    if ($size < 1024) {
-        return $size .' B';
-    } elseif ($size < 1048576) {
-        return round($size / 1024, 2) .' kB';
-    } elseif ($size < 1073741824) {
-        return round($size / 1048576, 2) . ' MB';
-    } elseif ($size < 1099511627776) {
-        return round($size / 1073741824, 2) . ' GB';
-    } elseif ($size < 1125899906842624) {
-        return round($size / 1099511627776, 2) .' TB';
-    } 
-}
-
 while ( $row = mysql_fetch_assoc( $res ) ) {
     $cache_id = $row['id'];
     $ip = $row['ip'];
     $status = $row['status'];
     $last_heartbeat = $row['last_heartbeat'];
-    $space_total = humanizeSize($row['space_total']);
-    $space_used = humanizeSize($row['space_used']);
-    echo "<tr><td>$cache_id</td><td>$ip</td><td>$status</td><td>$last_heartbeat</td><td>$space_total</td><td>$space_used</td></tr>"; 
+    echo "<tr><td>$cache_id</td><td>$ip</td><td>$status</td><td>$last_heartbeat</td></tr>";
 }
 
 ?>
 </table>
 </body>
-
