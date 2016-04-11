710ae24713a1234191ad31107fbc02d6f8fa42fb
Marius Mandal <mariusmandal@gmail.com>
2016-04-11 21:40:43 +0200

    perpage-korrigering

--- src/UKMNorge/UKMtvBundle/Controller/CronController.php
+++ src/UKMNorge/UKMtvBundle/Controller/CronController.php
@@ -70,6 +70,7 @@ class CronController extends Controller
 		$perpage = 500;
 		$stop = $perpage*$page;
 		$start = $stop-$perpage;
+		$perpage++; // Korriger så man ikke hopper over en per pageload
 		$sql = new SQL("SELECT * FROM `ukm_tv_files` WHERE `tv_id` > '$start' AND `tv_id` < '$stop' LIMIT $perpage");
 		$res = $sql->run();
 		
