f150527dca78910e763231493bcb7e52a6497dc9
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-01-23 20:15:14 +0100

    Endring på strukturering og filmer på home.html.twig
    Endrer litt på design og viser siste filmer på home side på ukmtv

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,13 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use UKMNorge\Filmer\UKMTV\Filmer;
 
 require_once('UKM/Autoloader.php');
 
 class HomeController extends AbstractController{
     public function index() {
-
+        
         #var_dump($this);die();
-        return $this->render('Front/Home.html.twig');
+        return $this->render(
+            'Front/Home.html.twig',
+            ['filmer' => Filmer::getLatest(20)->getAll()]
+        );
     }
 }
