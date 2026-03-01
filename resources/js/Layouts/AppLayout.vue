7918cc49527131e1bc7fbac01bce27c524d387f5
Jardar Nordbo <jardar@ukm.no>
2026-03-01 13:15:48 +0000

    logo to the right

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -2,7 +2,7 @@
     <div class="min-h-screen relative">
         <a
             href="https://ukm.no"
-            class="fixed left-4 top-4 sm:left-8 sm:top-6 z-50 inline-flex items-center text-slate-100 hover:text-purple-200 transition"
+            class="fixed right-4 top-4 sm:right-8 sm:top-6 z-50 inline-flex items-center text-slate-100 hover:text-purple-200 transition"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Gå til UKM.no"
